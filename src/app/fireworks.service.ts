5a305142aa8b742ae68eda6380454097f9395a6d
hongyevgeny <hongyevgeny@gmail.com>
2018-11-20 23:33:31 +0900

    auth update 1

--- src/app/fireworks.service.ts
+++ src/app/fireworks.service.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@angular/core';
 
 // firebase 도구 여기서는 모듈을 쓰지 않는다.
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AnhularFireAuth } from '@angular/fire/auth'
 import { Observable } from 'rxjs';
 
 export interface Item { // 필드를 설정하는건데 개별적 관리를 위해서는 ID 가 필요.
   id?:string;// 사용자가 만들때 디비에 어떤 ID 를 넣을 지 모르니까 예외처리
   name: string;
 }
+
+// export interface User{
+//   uid?:string;
+//
+// }
 
 @Injectable({
   providedIn: 'root'
@@ -16,13 +22,19 @@ export class FireworksService {
   private itemCollection: AngularFirestoreCollection<Item>;
   // 여기서 컬랙션은 mySql 에서의 테이블과 유사하다 논sql 은 나도 잘 모르겟다...
   items: Observable<Item[]>;// Item 목록
-
+  user: Observable<any[]>;
   constructor(
-    private readonly db: AngularFirestore
+    private readonly db: AngularFirestore,
+    private readonly auth: AngularFireAuth,
   ) {
     // db collection 에서 기능을 가져온다.
     this.itemCollection = db.collection<Item>('items');
     this.items = this.itemCollection.valueChanges();
+    this.user = auth.user;
+  }
+
+  getUser():Observable<any[]>{
+    return this.user;
   }
 
   getItems(): Observable<Item[]>{
@@ -40,5 +52,7 @@ export class FireworksService {
   delItem(id:string){// 가져올때는 doc 으로 가져온다.
     this.itemCollection.doc(id).delete();
   }
+
+
 
 }
